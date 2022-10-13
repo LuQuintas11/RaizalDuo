3c8ab0810aff18746db7507f45fde96c8285d3ad
LuQuintas11 <luciaquintas14@gmail.com>
2022-10-13 06:41:35 +0000

    Add view for likes

--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
    
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-   
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
     path('YouTubeApi.html/', views.YouTube)
     
     
