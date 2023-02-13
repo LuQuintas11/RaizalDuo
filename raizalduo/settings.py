58a3796e6bb03888240db77210a33419f3cf9c0f
LuQuintas11 <luciaquintas14@gmail.com>
2023-02-13 13:26:12 +0000

    added media root

--- raizalduo/settings.py
+++ raizalduo/settings.py
@@ -137,7 +137,7 @@ USE_L10N = True
 USE_TZ = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
